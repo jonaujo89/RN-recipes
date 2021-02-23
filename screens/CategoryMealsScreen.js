cb50a9f1147af0a988afa5bb56ebd53889d968cf
jonaujo89 <jonaujo89@gmail.com>
2021-02-23 18:47:07 -0500

    added simple screens, fonts, appLoading

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const CtegoryMealsScreen = () => {
+
+    return (
+        <View style={styles.screen}>
+            <Text>CtegoryMealsScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+export default CtegoryMealsScreen;
